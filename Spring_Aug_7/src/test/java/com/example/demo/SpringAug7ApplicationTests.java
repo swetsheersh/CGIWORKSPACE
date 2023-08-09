1a42c8317e0f3c0dab1ac08b89469e10ddbcff6a
Swet.kr.mis <swet.kr.mis@gmail.com>
2023-08-09 16:39:21 +0530

    9 Aug. Updated

--- Spring_Aug_7/src/test/java/com/example/demo/SpringAug7ApplicationTests.java
+++ Spring_Aug_7/src/test/java/com/example/demo/SpringAug7ApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
 @SpringBootTest
 class SpringAug7ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		baseURI="http://localhost:8080//getall";
+		given().get().then().statusCode(200);
 	}
 
 }
