1004c755c10d0940d3bc3bd7869ab063184bf417
Swet.kr.mis <swet.kr.mis@gmail.com>
2023-08-22 17:00:15 +0530

    22 aug updated

--- PageObjectModel/src/test/java/SeleniumPOM/Assign/PageObjectModel/RegisterTest.java
+++ PageObjectModel/src/test/java/SeleniumPOM/Assign/PageObjectModel/RegisterTest.java
@@ -3,11 +3,17 @@ package SeleniumPOM.Assign.PageObjectModel;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 
+import static org.testng.Assert.assertEquals;
 
+import java.io.FileInputStream;
 import java.time.Duration;
 
-
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,25 +25,29 @@ public class RegisterTest {
 	WebDriver driver;
 	Register reg;
 	
-  @Test
-  public void verifyRegister() throws Exception {
-	  driver.get("https://www.facebook.com/");
-	  driver.manage().window().maximize();
+  @Test(dataProvider = "dp")
+  public void verifyRegister(String fname, String lname,String email,String reemail, String pass,String day,String mon, String year,String gen,String exp) throws Exception {
+	  //driver.get("https://www.facebook.com/");
+	  //driver.manage().window().maximize();
+	  
+	  driver.navigate().to("https://www.facebook.com/");
 	  
 	 // Thread.sleep(3000);
 	  //reg=new Register(driver);
 	  reg.create();
-	  reg.sendfirstName("Saurav");
-	  reg.sendlastName("CGI");
-	  reg.sendEmail("saurav@gmail.com");
-	  reg.resendEmail("saurav@gmail.com");
-	  reg.sendpass("Test@123");
-	  reg.sendday("12");
-	  reg.sendmonth("5");
-	  reg.sendyear("1990");
-	  reg.clickgender("Custom");
+	  reg.sendfirstName(fname);
+	  reg.sendlastName(lname);
+	  reg.sendEmail(email);
+	  reg.resendEmail(reemail);
+	  reg.sendpass(pass);
+	  reg.sendday(String.valueOf((int)Double.parseDouble(day)));
+	  reg.sendmonth(String.valueOf((int)Double.parseDouble(mon)));
+	  reg.sendyear(String.valueOf((int)Double.parseDouble(year)));
+	  reg.clickgender(gen);
 	  reg.clickSubmit();
-	  
+	  Thread.sleep(6000);
+	  assertEquals(driver.getCurrentUrl().contains("recover"), Boolean.parseBoolean(exp));
+	  Thread.sleep(2000);
   }
   @BeforeMethod
   public void beforeMethod() {
@@ -52,11 +62,40 @@ public class RegisterTest {
 	  ChromeOptions options=new ChromeOptions();
 	  options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
 	  this.driver=new ChromeDriver(options);
+	  driver.get("https://www.facebook.com/");
+	  driver.manage().window().maximize();
+  }
+  
+  @DataProvider
+  public Object[][] dp() throws Exception {
+	  FileInputStream file=new FileInputStream("C:/Users/HARSH/Desktop/fb.xlsx");
+  	  XSSFWorkbook workbook =new XSSFWorkbook(file);
+  	  XSSFSheet sheet=workbook.getSheetAt(0);
+  	  int rowNum=sheet.getLastRowNum();
+  	  int colNum=sheet.getRow(0).getLastCellNum();
+  	  Object[][] myobj = new Object[rowNum][colNum];
+  	  for(int i=1;i<=rowNum;i++) {
+  		  XSSFRow row=sheet.getRow(i);
+  		  for(int j=0;j<colNum;j++) {
+  			  try {
+  			  String value=row.getCell(j).toString();
+  			  System.out.print(value+" ");
+  			  myobj[i-1][j]=value;
+  			  }catch(Exception e) {}
+  		  }
+  		  //row.createCell(colNum).setCellValue("Pass");
+  		  
+  		  System.out.println("");
+  	  }
+  	 // OutputStream file1=new FileOutputStream("C:/Users/HARSH/Desktop/Test.xlsx");
+  	 // workbook.write(file1);
+  	 // file1.close();
+    return myobj;
   }
 
   @AfterSuite
   public void afterSuite() {
-	 // this.driver.quit();
+	  this.driver.quit();
   }
 
 }
