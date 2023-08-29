5a4904495acd0c8debf661f74f4bb4fd0c05f1ee
Swet.kr.mis <swet.kr.mis@gmail.com>
2023-08-29 14:59:03 +0530

    29 Aug updated

---
+++ springBootCi_Cd_25Aug-1/src/main/java/com/example/docker/SpringBootCiCd25Aug1Application.java
@@ -0,0 +1,13 @@
+package com.example.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCiCd25Aug1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCiCd25Aug1Application.class, args);
+	}
+
+}
